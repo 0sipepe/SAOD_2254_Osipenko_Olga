dd1bf98d3e6b65f5671ddd5a2f00c8d5b10f47bc
olga <osipenko270104@mail.ru>
2023-12-16 13:38:00 +0300

    Сдача задания хеш-таблицы, перебор не работает

---
+++ task_HashTable/task_HashTable/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_HashTable
+{
+    public partial class Form1 : Form
+    {
+        MyHashTable<string, string> table = new MyHashTable<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                table.Add(tb_AddKey.Text, tb_AddValue.Text);
+                lb_Count.Text = table.CountElem.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Table already contains that value with that key");
+            }
+            
+        }
+
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+                table.Remove(tb_Remove.Text);
+                lb_Count.Text = table.CountElem.ToString();
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("No such value");
+            //}
+        }
+
+        private void btn_ValueByKey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tb_VBKValue.Text = table.ValueByKey(tb_VBKKey.Text);
+            }
+            catch
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private void btn_ContainsKey_Click(object sender, EventArgs e)
+        {
+            if (table.ContainsKey(tb_ContainsKey.Text))
+            {
+                btn_ContainsKey.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                btn_ContainsKey.BackColor = Color.Red;
+            }
+        }
+
+        private void btn_KBV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tb_KBVKey.Text = table.KeyByValue(tb_KBVValue.Text);
+            }
+            catch
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private void btn_ContainsValue_Click(object sender, EventArgs e)
+        {
+            if (table.ContainsValue(tb_ContainsValue.Text))
+            {
+                btn_ContainsValue.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                btn_ContainsValue.BackColor = Color.Red;
+            }
+        }
+
+        private void btn_ListKeys_Click(object sender, EventArgs e)
+        {
+            List<string> keys = table.GetKeys();
+            foreach(string key in keys)
+            {
+                tb_ListKeys.Text += key + " ";
+            }
+        }
+
+        private void btn_ListValues_Click(object sender, EventArgs e)
+        {
+            List<string> values = table.GetValues();
+            foreach(string val in values)
+            {
+                tb_ListValue.Text += val + " ";
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            table.Clear();
+            lb_Count.Text = table.Count.ToString();
+        }
+    }
+}
---
+++ task_HashTable/task_HashTable/MyHashTable.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_HashTable
+{
+    internal class MyHashTable<TKey, TValue> : IEnumerable
+    {
+        List<LinkedList<KeyValuePair<TKey, TValue>>> data = new List<LinkedList<KeyValuePair<TKey, TValue>>>(100);
+        int countElem = 0;
+        int count;
+
+        public MyHashTable()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                data.Add(new LinkedList<KeyValuePair<TKey, TValue>>());
+            }
+        }
+        private int GetMyHashCode(TKey key)
+        {
+            return Math.Abs(key.GetHashCode() % data.Capacity);
+        }
+        public int Count
+        {
+
+            get 
+            { 
+                foreach(LinkedList < KeyValuePair < TKey, TValue >> list in data)
+                {
+                    if (list.Count != 0 )
+                    {
+                        count++;
+                    }
+                }
+                return count; 
+            }
+        }
+        public int CountElem
+        {
+            get { return countElem; }
+        }
+        public void Add(TKey key, TValue value)
+        {
+            if (!(ContainsPair(key, value)))
+            {
+                Console.WriteLine(GetMyHashCode(key));
+                data[GetMyHashCode(key)].AddLast(new KeyValuePair<TKey, TValue>(key, value));
+                countElem++;
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+        public void Remove(TKey key)
+        {
+            if (!ContainsKey(key))
+            {
+                throw new Exception();
+            }
+            KeyValuePair<TKey, TValue> p = new KeyValuePair<TKey, TValue>();
+            int index = GetMyHashCode(key);
+            foreach (KeyValuePair<TKey, TValue> pair in data[index])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    data[index].Remove(pair);
+                    countElem--;
+                    break;
+                }
+            }
+            
+
+
+        }
+        public void Clear()
+        {
+            data.Clear();
+        }
+        public TValue ValueByKey(TKey key)
+        {
+            foreach(KeyValuePair<TKey, TValue> pair in data[GetMyHashCode(key)])
+            {
+                if (pair.Key.Equals(key))
+                {
+                    return pair.Value;
+                }
+            }
+            throw new Exception();
+        }
+        public TKey KeyByValue(TValue value)
+        {
+            foreach (LinkedList<KeyValuePair<TKey, TValue>> list in data)
+            {
+                foreach(KeyValuePair<TKey, TValue> pair in list)
+                {
+                    if (pair.Value.Equals(value))
+                    {
+                        return pair.Key;
+                    }
+                }
+                    
+            }
+            throw new Exception();
+        }
+
+        private bool ContainsPair(TKey key, TValue value)
+        {
+            foreach (LinkedList<KeyValuePair<TKey, TValue>> list in data)
+            {
+                foreach (KeyValuePair<TKey, TValue> pair in list)
+                {
+                    if (pair.Value.Equals(value) && pair.Key.Equals(key))
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+        public TValue this[TKey index]
+        {
+            get { return ValueByKey(index); }
+        }
+        public bool ContainsKey(TKey key)
+        {
+            try
+            {
+                this.ValueByKey(key);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            try
+            {
+                this.KeyByValue(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public List<TKey> GetKeys()
+        {
+            List<TKey> list = new List<TKey>();
+            foreach(KeyValuePair<TKey, TValue> pair in this)
+            {
+                list.Add(pair.Key);
+            }
+            return list;
+        }
+
+        public List<TValue> GetValues()
+        {
+            List<TValue> list = new List<TValue>();
+            foreach(KeyValuePair<TKey, TValue> pair in this)
+            {
+                list.Add(pair.Value);
+            }
+            return list;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new MyTableEnum(data, Count);
+        }
+        public class MyTableEnum : IEnumerator
+        {
+                      
+            int listIndex = 0;
+            int pairIndex = 0;
+            int count = 0;
+            List<LinkedList<KeyValuePair<TKey, TValue>>> data;
+
+            public MyTableEnum(List<LinkedList<KeyValuePair<TKey, TValue>>> data,int count)
+            {
+                this.data = data;
+                this.count = count;
+            }
+            public bool MoveNext()
+            {
+               
+                if (listIndex < data.Count)
+                {
+                    if (pairIndex < data[listIndex].Count - 1)
+                    {
+                        pairIndex++;
+                        return true;
+                    }
+                    while (data[listIndex].Count == 0)
+                    {
+                        pairIndex = 0;
+                        listIndex++;
+                    }
+                    
+                    return true;
+                }
+                return false;
+               
+            }
+            public void Reset()
+            {
+                pairIndex = 0;
+                listIndex = 0;
+            }
+            public object Current
+            {
+                get { return data[listIndex].ElementAt(pairIndex); }
+
+            }
+
+        }
+    }
+}
